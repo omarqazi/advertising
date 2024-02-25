a44c9474cc1df6a606d33285fc8a797dabf1c1a5
Omar Qazi <omar@smick.com>
2024-02-25 01:19:50 -0800

    configure devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'wholemars.com' }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
