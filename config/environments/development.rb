968d3dfe4070001ede402f8e02a8e82d71c4f930
Omar Qazi <omar@smick.com>
2024-02-25 02:04:51 -0800

    got bare bones devise auth working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+	  address:              'smtp.sendgrid.net',
+	  port:                 587,
+	  domain:               'wholemars.com',
+	  user_name:            'apikey',
+	  password:             ENV["SENDGRID_API_KEY"],
+	  authentication:       'plain',
+	  enable_starttls_auto: true,
+	  open_timeout:         5,
+	  read_timeout:         5 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'wholemars.com' }
+  config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+	  address:              'smtp.sendgrid.net',
+	  port:                 587,
+	  domain:               'spoonsword.com',
+	  user_name:            'apikey',
+	  password:             ENV["SENDGRID_API_KEY"],
+	  authentication:       'plain',
+	  enable_starttls_auto: true,
+	  open_timeout:         5,
+	  read_timeout:         5 }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
