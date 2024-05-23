9227ed31ca4d1bfe5d4d8e93ec20389f76952e8e
Omar Qazi <omar@smick.com>
2024-05-23 01:34:24 -0700

    added first name, last name, and stripe account id fields to users table

---
+++ db/migrate/20240523083310_add_name_and_stripe_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAndStripeToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :stripe_customer_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_092838) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_083310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_092838) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "stripe_customer_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
