385f4f2db05799d77def398d2255ea3eb2efdc97
Omar Qazi <omar@smick.com>
2024-05-23 01:11:05 -0700

    use the correct subdomain in the mailer so that confirmation links work properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'wholemars.com' }
+  config.action_mailer.default_url_options = { host: 'adcontest.wholemars.com' }
   config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
 	  address:              'smtp.sendgrid.net',
