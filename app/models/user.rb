462288028bd96dd0ab7dbdff7c857fa03fc81f2d
Omar Qazi <omar@smick.com>
2024-05-23 01:38:10 -0700

    use name if present, otherwise email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def has_name?
+    !(first_name.blank? && last_name.blank?)
+  end
+
+  def display_name
+    has_name? ? full_name : email
+  end
 end
