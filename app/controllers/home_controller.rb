58cd94c698dde3e620800151487f49e60dfc81cd
Omar Qazi <omar@smick.com>
2024-05-25 18:26:33 -0700

    added some rubocop fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# The HomeController handles requests to the homepage
+# It includes actions to display the home page
+
+class HomeController < ApplicationController
+	def index
+		# Action for the home page
+	end
+
+	# Add other actions here as needed
+end
+
 class HomeController < ApplicationController
 	def index
 		@user = current_user
