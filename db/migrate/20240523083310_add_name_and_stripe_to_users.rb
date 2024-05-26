02e8b770be7b791e179f19db34f11d883ab811c5
Omar Qazi <omar@smick.com>
2024-05-25 18:42:30 -0700

    all rubocop offenses finally actually fixed

--- db/migrate/20240523083310_add_name_and_stripe_to_users.rb
+++ db/migrate/20240523083310_add_name_and_stripe_to_users.rb
@@ -2,10 +2,10 @@
 
 class AddNameAndStripeToUsers < ActiveRecord::Migration[7.1]
 	def change
-    change_table :users, bulk: true do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :stripe_customer_id
-    end
+		change_table :users, bulk: true do |t|
+			t.string :first_name
+			t.string :last_name
+			t.string :stripe_customer_id
+		end
 	end
 end
