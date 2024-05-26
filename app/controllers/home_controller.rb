303d5b20e571b51185e808d3cb24244b9959f27c
Omar Qazi <omar@smick.com>
2024-05-25 18:28:30 -0700

    added rubocop fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,6 @@
 
 # The HomeController handles requests to the homepage
 # It includes actions to display the home page
-
-class HomeController < ApplicationController
-	def index
-		# Action for the home page
-	end
-
-	# Add other actions here as needed
-end
-
 class HomeController < ApplicationController
 	def index
 		@user = current_user
